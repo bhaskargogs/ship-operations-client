8eebd827a39f2384efa874b56f7f58c78ca21ca0
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-04-17 11:11:24 +0200

    Added Modal for Create operation

---
+++ src/components/CreateShipModal.js
@@ -0,0 +1,133 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
+
+
+
+const CreateShipModal = props => {
+    const [ form, setForm ] = useState({})
+    const [ errors, setErrors ] = useState({})
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const newErrors = findFormErrors()
+    // Conditional logic:
+        if ( Object.keys(newErrors).length > 0 ) {
+      // We got errors!
+            setErrors(newErrors)
+        } else {
+            let result = await axios.post("http://localhost:8080/ships", 
+            { 
+                name: event.target.name.value,
+                length: event.target.length.value,
+                width: event.target.width.value,
+                code: event.target.code.value
+             });
+             alert('')
+        }
+    }
+    const setField = (field, value) => {
+    setForm({
+      ...form,
+      [field]: value
+    })
+  }
+  const findFormErrors = () => {
+    const { name, length, width, code } = form
+    const newErrors = {}
+    // name errors
+    if ( name.length > 50 ) newErrors.name = 'Name is too long!'
+    const expr = /^[0-9]*\.?[0-9]*$/
+    // length errors
+    if ( !expr.test(length) ) newErrors.length = 'Length is not valid!'
+    if ( !expr.test(width) ) newErrors.width = 'Width is not valid!'
+    const codeExpr = /^[A-Z]{4}-\d{4}-[A-Z]{1}\d{1}/
+    if ( !codeExpr.test(code) ) newErrors.code = 'Code is not valid (format: AAAA-0000-A0)!'
+
+    return newErrors
+    }
+
+    return (
+        <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Create Ship
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="container">
+        <Row>
+            <Col sm={6}>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="Name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            name="name" 
+                            onChange={ e => setField('name', e.target.value) } 
+                            required 
+                            isInvalid={ !!errors.name }
+                            placeholder="Name"/>
+                            <Form.Control.Feedback type='invalid'>
+                                { errors.name }
+                            </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="Length">
+                        <Form.Label>Length (metres)</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            name="length" 
+                            onChange={ e => setField('length', e.target.value) } 
+                            required 
+                            isInvalid={ !!errors.length }
+                            placeholder="Length"/>
+                            <Form.Control.Feedback type='invalid'>
+                                { errors.length }
+                            </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="Width">
+                        <Form.Label>Width (metres)</Form.Label>
+                        <Form.Control 
+                        type="text" 
+                        name="width" 
+                        onChange={ e => setField('width', e.target.value) } 
+                        required 
+                        isInvalid={ !!errors.width }
+                        placeholder="Width"/>
+                        <Form.Control.Feedback type='invalid'>
+                            { errors.width }
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="Code">
+                        <Form.Label>Code (AAAA-0000-A0)</Form.Label>
+                        <Form.Control 
+                        type="text" 
+                        name="code" 
+                        onChange={ e => setField('code', e.target.value) } 
+                        required 
+                        isInvalid={ !!errors.code }
+                        placeholder="Code"/>
+                        <Form.Control.Feedback type='invalid'>
+                            { errors.code }
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
+                        <Button variant="primary" type="submit">Create Ship</Button>
+                    </Form.Group>
+                </Form>
+            </Col>
+        </Row>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="danger" onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+    );
+}
+
+export default CreateShipModal;
--- src/components/Ships.js
+++ src/components/Ships.js
@@ -1,39 +1,52 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { ButtonToolbar, Button, Table } from 'react-bootstrap';
+import CreateShipModal from './CreateShipModal';
 
 const Ships = props => {
   const [ships, setShips] = useState([]);
-  useEffect(() => {
-    async function fetchShips() {
-      let result = await axios.get('http://localhost:8080/ships');
-      setShips(result.data);
-    }
-    fetchShips();
-  }, []);
+  const [addShowModal, setAddShowModal] = useState(false);
+  const fetchShips = async () => {
+    const response = await axios.get('http://localhost:8080/ships');
+
+    setShips(response.data);
+  };
+
+  useEffect(() => { fetchShips(ships) }, [ ships ]);
+
+  let onModalClose = () => setAddShowModal(false);
+
   return (
-    <Table className='mt-4' striped bordered hover size='small'>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Length (metres)</th>
-          <th>Width (metres)</th>
-          <th>Code</th>
-        </tr>
-      </thead>
-      <tbody>
-        {ships.map(ship => (
-          <tr key={ship.id}>
-            <td>{ship.id}</td>
-            <td>{ship.name}</td>
-            <td>{ship.length}</td>
-            <td>{ship.width}</td>
-            <td>{ship.code}</td>
+    <div>
+      <Table className='mt-4' striped bordered hover size='small'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Length (metres)</th>
+            <th>Width (metres)</th>
+            <th>Code</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {ships.map(ship => (
+            <tr key={ship.id}>
+              <td>{ship.id}</td>
+              <td>{ship.name}</td>
+              <td>{ship.length}</td>
+              <td>{ship.width}</td>
+              <td>{ship.code}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <ButtonToolbar>
+        <Button variant='primary' onClick={() => setAddShowModal(true)}>
+          Create Ship
+        </Button>
+        <CreateShipModal show={addShowModal} onHide={onModalClose} />
+      </ButtonToolbar>
+    </div>
   );
 };
 
